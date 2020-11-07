03878509306e507170a6e7ef59f1c268cdd93376
Aseem <aseemanand2@gmail.com>
2020-11-07 23:55:12 +0530

    [Aseem].[Refactor] Added code to find cheapest best rated hotel for any customer type and throw custom exceptions

--- HotelReservationSystem/HotelReservation.cs
+++ HotelReservationSystem/HotelReservation.cs
@@ -51,9 +51,10 @@ namespace HotelReservationSystem
                     {
                         Console.WriteLine($"Hotel {hotelName} already exists in the records\n");
                     }
-                    break;
+                    break;                    
+                default:
+                    throw new HotelReservationCustomException(HotelReservationCustomException.ExceptionType.INVALID_CUSTOMER_TYPE, "INVALID CUSTOMER TYPE");
             }
-            
         }
 
         /// <summary>
@@ -69,8 +70,11 @@ namespace HotelReservationSystem
                 Console.WriteLine("Enter the check-in date(DDMMMYYYY):");
                 DateTime checkinDate = DateTime.Parse(Console.ReadLine());
                 Console.WriteLine("Enter the check-out date(DDMMMYYYY):");
-                DateTime checkoutDate = DateTime.Parse(Console.ReadLine());  
+                DateTime checkoutDate = DateTime.Parse(Console.ReadLine()); 
                 
+                if(checkinDate>checkoutDate)
+                    throw new HotelReservationCustomException(HotelReservationCustomException.ExceptionType.INVALID_DATE_RANGE, "CHECKOUT DATE MUST BE AHEAD OF CHECKIN DATE");
+
                 Dictionary<string, OutputHotel> rateRecordsForRegularCustomer = new Dictionary<string, OutputHotel>();
                 Dictionary<string, OutputHotel> rateRecordsForRewardsCustomer = new Dictionary<string, OutputHotel>();
 
@@ -129,12 +133,11 @@ namespace HotelReservationSystem
                     return rateRecordsForRewardsCustomer;
                 }
                 else
-                    return null;
-                
+                    throw new HotelReservationCustomException(HotelReservationCustomException.ExceptionType.INVALID_CUSTOMER_TYPE, "INVALID CUSTOMER TYPE");                
             }
-            catch (Exception e)
+            catch
             {
-                throw new Exception(e.Message);
+                throw new HotelReservationCustomException(HotelReservationCustomException.ExceptionType.INVALID_DATE_FORMAT, "INVALID DATE FORMAT");
             }
         }
 
@@ -162,7 +165,8 @@ namespace HotelReservationSystem
         /// <param name="ratings">The ratings.</param>
         public static void AddRatings(string hotelName,int ratings)
         {
-            foreach(var v in hotelRecordsRegularCustomer)
+            //Adding ratings in dictionary for regular customers
+            foreach (var v in hotelRecordsRegularCustomer)
             {
                 if (v.Key == hotelName)
                 {
@@ -170,6 +174,15 @@ namespace HotelReservationSystem
                     break;
                 }
             }
+            //Adding ratings in dictionary for rewards customer
+            foreach (var v in hotelRecordsRewardsCustomer)
+            {
+                if (v.Key == hotelName)
+                {
+                    v.Value.ratings = ratings;
+                    break;
+                }
+            }            
         }
 
         /// <summary>
@@ -237,7 +250,5 @@ namespace HotelReservationSystem
             AddRatings("Lakewood", 3);
             AddRatings("Ridgewood", 5);
         }
-
-
     }
 }
